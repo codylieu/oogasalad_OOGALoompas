507c2dc59057900756260fc1838957424721d3c9
Lawrence Lin <justlah@gmail.com>
2014-03-28 16:05:12 -0400

    Starter code for Monster

--- src/main/java/engine/objects/Monster.java
+++ src/main/java/engine/objects/Monster.java
@@ -1,4 +1,19 @@
 package engine.objects;
 
 public class Monster extends TDObject {
+	
+	/** Create Monster Objects
+	 * @param name
+	 * @param x
+	 * @param y
+	 * @param gfxname
+	 * @param health
+	 * @param damage
+	 * @param range
+	 */
+	public Monster(String name, double x, double y, String gfxname, double health, double damage, double range) {
+	
+		
+		
+	}
 }
