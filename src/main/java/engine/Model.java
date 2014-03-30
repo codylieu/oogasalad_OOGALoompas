a3c2cfc4af36b8f1373ed284dd48b4e2da762f36
Lawrence Lin <justlah@gmail.com>
2014-03-30 02:43:51 -0400

    Starter code for Model

--- src/main/java/engine/Model.java
+++ src/main/java/engine/Model.java
@@ -1,4 +1,12 @@
 package main.java.engine;
+import java.util.List;
+
+import main.java.engine.objects.TDObject;
 
 public class Model {
+	private List<TDObject> TDObjects;
+	
+	public Model() {
+		
+	}
 }
