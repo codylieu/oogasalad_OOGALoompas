da4b4538e5cbde644abb65cf942061ddb7c2fdbe
Lawrence Lin <justlah@gmail.com>
2014-03-30 02:43:06 -0400

    Starter code for Player

---
+++ src/main/java/engine/Player.java
@@ -0,0 +1,43 @@
+package main.java.engine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Player {
+	private double money;
+	private int health;
+	private double score;
+	private Map<String, Integer> skills;
+	
+	public Player() {
+		this.money = 0;
+		this.health = 3;
+		this.score = 0;
+		this.skills = new HashMap<String, Integer>();
+	}
+	
+	public void updateMoney(double money) {
+		this.money += money;
+	}
+	
+	public void updateHealth() {
+		this.health -= 1;
+	}
+	
+	public void updateScore(double score) {
+		this.score += score;
+	}
+	
+	public void updateSkills(String skill) {
+		if (this.skills.containsKey(skill)) this.skills.put(skill, this.skills.get(skill)+1);
+		else this.skills.put(skill, 1);
+	}
+	
+	public double getMoney() {
+		return this.money;
+	}
+	
+	public double getHealth() {
+		return this.health;
+	}
+}
