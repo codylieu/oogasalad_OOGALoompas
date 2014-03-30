41ef71eb5431ca928bd0ec46a81dc98d3f8ac56c
In-Young Jo <ij8@duke.edu>
2014-03-30 04:49:41 -0400

    working on serializer and parser

--- src/main/java/data/jsonhandler/JSONParser.java
+++ src/main/java/data/jsonhandler/JSONParser.java
@@ -1,10 +1,16 @@
 package java.data.jsonhandler;
 
+import java.data.datahandler.DataBundle;
+
 public class JSONParser {
 	
 	public JSONParser()	{
 		
 	}
 	
+	public DataBundle readFile(String filename){
+		DataBundle output = new DataBundle();
+		return output;
+	}
 	
 }
--- src/main/java/data/jsonhandler/JSONSerializer.java
+++ src/main/java/data/jsonhandler/JSONSerializer.java
@@ -1,10 +1,25 @@
 package java.data.jsonhandler;
 
+import java.data.datahandler.DataBundle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
 public class JSONSerializer {
 
 	public JSONSerializer(){
 		
 	}
 	
+	public void write(String filename, DataBundle d) throws FileNotFoundException	{
+		File outputFile = new File("resources/" + filename + ".json");
+		PrintWriter output = new PrintWriter(outputFile);
+		output.println("{");
+		for(Class<?> c : d.getDataMap().keySet())	{
+			output.println('"' + c.toString() + '"' + ":{");
+			List<Object> list = d.getList(c);
+		}
+	}
 	
 }
