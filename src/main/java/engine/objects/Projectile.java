74dfddbf3783fedb7bc89bc0a0a1e2713603fc0b
Austin Lu <austin.lu@duke.edu>
2014-03-31 02:58:39 -0400

    Add projectile class, engine images resources folder, and image

---
+++ src/main/java/engine/objects/Projectile.java
@@ -0,0 +1,38 @@
+package main.java.engine.objects;
+
+import jgame.JGObject;
+
+
+public class Projectile extends TDObject {
+
+    public static final int TOWER_PROJECTILE_CID = 10;
+    public static final double DEFAULT_SPEED = 20;
+
+    /**
+     * Create projectile with specific src coordinates, xspeed, and yspeed
+     * @param x src x-coor
+     * @param y src y-coor
+     * @param xspeed
+     * @param yspeed
+     */
+    public Projectile (double x, double y, double xspeed, double yspeed) {
+        super("projectile", x, y, TOWER_PROJECTILE_CID, "red_bullet", xspeed, yspeed,
+              JGObject.expire_off_view);
+    }
+
+    /**
+     * Creates projectile heading in given angle with default speed.
+     * 
+     * @param x src x-coor
+     * @param y src y-coor
+     * @param angle Math.atan2(destX - srcX, destY - srcY)
+     */
+    public Projectile (double x, double y, double angle) {
+        super("projectile", x, y, TOWER_PROJECTILE_CID, "red_bullet",
+              DEFAULT_SPEED * Math.sin(angle),
+              DEFAULT_SPEED * Math.cos(angle),
+              JGObject.expire_off_view);
+
+    }
+
+}
