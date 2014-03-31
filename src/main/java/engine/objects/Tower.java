04fb7a9b3a0737ea00217f7173abb68c26014de7
Austin Lu <austin.lu@duke.edu>
2014-03-31 03:05:45 -0400

    Update tower constructor and add fireProjectile()

--- src/main/java/engine/objects/Tower.java
+++ src/main/java/engine/objects/Tower.java
@@ -1,12 +1,38 @@
 package main.java.engine.objects;
 
+import jgame.JGObject;
+import ProjectPoseidon.Constants;
 import main.java.engine.objects.TDObject;
+
 
 public class Tower extends TDObject {
 
-	public Tower(String name, boolean unique_id, double x, double y,
-			int collisionid, String gfxname) {
-		super(name, unique_id, x, y, collisionid, gfxname);
-		// TODO Auto-generated constructor stub
-	}
+    public static final String TOWER_GFX = "tower";
+    public static final int TOWER_CID = 0;
+
+    private double health;
+    private double damage;
+    private double range;
+
+    /**
+     * Create a tower at the specified x,y coordinate.
+     * @param x
+     * @param y
+     */
+    public Tower (double x, double y) {
+        super("tower", x, y, TOWER_CID, TOWER_GFX);
+    }
+
+    /**
+     * Shoot a projectile in the direction of the specified x,y target coordinates
+     * 
+     * @param targetX
+     * @param targetY
+     */
+    public void fireProjectile (double targetX, double targetY) {
+        /* trigonometry from Guardian JGame example */
+        double angle = Math.atan2(targetX - this.x, targetY - this.y);
+        new Projectile(this.x, this.y, angle);
+    }
+
 }
