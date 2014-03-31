af4dc1bc8fc129cf78807722c92f95a8e6994c27
sdh31 <sdh31@duke.edu>
2014-03-31 09:51:32 -0400

    javadocs on gui playback

--- src/test/java/author/GUIAutomation.java
+++ src/test/java/author/GUIAutomation.java
@@ -31,44 +31,63 @@ import main.java.author.view.AuthoringView;
 
 public class GUIAutomation extends JPanel{
 	private static Robot robot;
-	private List<Integer> mouseXPos;
-	private List<Integer> mouseYPos;
-	private static final int CLICK = -1;
-	private static final int DRAG = -2;
-	private boolean mousePressed;
-	private boolean mouseReleased;
-	private boolean shouldUpdatePos;
-	private boolean hasBeenClicked;
-	private int mousePressedX;
-	private int mousePressedY;
-	private int mouseReleasedX;
-	private int mouseReleasedY;
-	private boolean isRecording;
+	private static final int CLICK = -1; // 'special' value written to .txt file to recognize a mouse click
+	private static final int DRAG = -2; // 'special' value written to .txt file to recognize a mouse drag
+	private static final int MAX_RECORD_TIME = 25; // (in seconds) can be changed, but kept small so we don't
+	                                               // write too much data to the .txt file
+	private List<Integer> mouseXPos; // list of mouse movements, clicks, and drags
+	private List<Integer> mouseYPos; // list of mouse movements, clicks, and drags
+	private boolean mousePressed;    // is the mouse currently pressed down
+	private boolean mouseReleased;   // has the mouse recently been released from a click/drag
+	private boolean shouldUpdatePos; // determines whether we should update the position of the initial mouse press 
+	                                 // (only should update after a mouse click)
+	private boolean hasBeenClicked;  // has the mouse recently been clicked
+	private boolean isRecording;     // is the program currently recording 
+	private int mousePressedX;       // x position of mouse upon its initial press
+	private int mousePressedY;       // y position of mouse upon its initial press
+	private int mouseReleasedX;      // x position of mouse upon its release
+	private int mouseReleasedY;      // y position of mouse upon its release
 	
+	/**
+	 * Executes the recording (really obtaining data for the logging) of mouse movements, clicks, drags
+	 */
 	private void record() {
-		System.out.println("Recording..");
 		isRecording = true;
 		long initTime = System.currentTimeMillis() / 1000;
 		long currTime;
 		mouseXPos = new ArrayList<Integer>();
 		mouseYPos = new ArrayList<Integer>();
-		while (isRecording && (currTime = System.currentTimeMillis() / 1000) < 25 + initTime) {
+		
+		while (isRecording && (currTime = System.currentTimeMillis() / 1000) < MAX_RECORD_TIME + initTime) {
+			 // obtaining mouse absolute location
 			int xLoc = MouseInfo.getPointerInfo().getLocation().x;
 			int yLoc = MouseInfo.getPointerInfo().getLocation().y;
+			
+			// we typically prevent marking the position of identical, consecutive points as we are concerned
+			// with mouse movement, rather than a stationary mouse. In addition, failing to do so results in
+			// extremely slow playback time as we would be taking measurements of the mouse position at a very high frequency
 			boolean shouldMark = (mouseXPos.size() == 0) ? true : !(xLoc == mouseXPos.get(mouseXPos.size() - 1)
-					&& yLoc == mouseYPos.get(mouseYPos.size() - 1));
+					&& yLoc == mouseYPos.get(mouseYPos.size() - 1)); 
+			                                                 
 
+			// the code below distinguishes between a mouse click and a mouse drag (which is tricky). 
+			// when the mouse is pressed down, we obtain the position of the mouse when it was pressed
 			if (shouldUpdatePos) {
 				mousePressedX = MouseInfo.getPointerInfo().getLocation().x;
 				mousePressedY = MouseInfo.getPointerInfo().getLocation().y;
 				shouldUpdatePos = false;
 			}
+			
+			// when the mouse is released, we obtain the position of the mouse when it was released
 			if (mouseReleased) {
 				mouseReleasedX = MouseInfo.getPointerInfo().getLocation().x;
 				mouseReleasedY = MouseInfo.getPointerInfo().getLocation().y;
 			}
 			
+			// after the mouse has been pressed and released, we must determine whether this was a drag,
+			// or just a click
 			if (hasBeenClicked) {
+				// logic to determine if press and release was a click
 				if (mousePressedX == mouseReleasedX && mousePressedY == mouseReleasedY) {
 					mouseXPos.add(CLICK);
 					mouseYPos.add(CLICK);
@@ -79,6 +98,9 @@ public class GUIAutomation extends JPanel{
 				hasBeenClicked = false;
 			}
 			
+			// we only mark the movement of the the mouse of the mouse is not currently pressed down.
+			// if the mouse is pressed down, we may currently be in a drag. If so, the details of the
+			// dragging are handled in GUIAutomationPlayback.java
 			if (shouldMark && !mousePressed) {
 				mouseXPos.add(MouseInfo.getPointerInfo().getLocation().x);
 				mouseYPos.add(MouseInfo.getPointerInfo().getLocation().y);
@@ -88,6 +110,10 @@ public class GUIAutomation extends JPanel{
 		logToFile();
 	}
 
+	/**
+	 * Logs information about the set of events that have occurred
+	 * during the recording
+	 */
 	private void logToFile() {
 		try {
 			File file = new File("src/test/java/author/test.txt");
@@ -107,7 +133,11 @@ public class GUIAutomation extends JPanel{
 		}
 	}
 
-	private void allowStop(Component c) {
+	
+	/**
+	 * Constructs a way in which the user can stop the 'recording'
+	 */
+	private void allowStop() {
 		JFrame stopFrame = new JFrame();
 		JButton stopButton = new JButton("STOP RECORDING");
 		stopButton.addActionListener(new ActionListener() {
@@ -125,65 +155,51 @@ public class GUIAutomation extends JPanel{
 		stopButton.setPreferredSize(new Dimension(200,100));
 		stopFrame.getContentPane().add(stopButton, BorderLayout.CENTER);
 		stopFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		stopFrame.setLocationRelativeTo(c);
+		stopFrame.setLocationRelativeTo(null); // offsets the JButton so it does not appear over user's view
 		stopFrame.pack();
 		stopFrame.setVisible(true);
 	}
 
+	
+	/**
+	 * Initializes java's Robot as a way of maintaining
+	 * track of the state of your program
+	 */
 	private void initRobot() {
 		try {
 			robot = new Robot();
 		} catch (AWTException e) { e.printStackTrace(); }
 	}
 
+	/**
+	 * Creates a global mouse listener, works regardless of the component the mouse clicks on
+	 */
 	private void initMouse() {
 		long eventMask = AWTEvent.MOUSE_EVENT_MASK;  
 
-		Toolkit.getDefaultToolkit().addAWTEventListener( new AWTEventListener()  
-		{  
-			public void eventDispatched(AWTEvent e)  
-			{  
+		Toolkit.getDefaultToolkit().addAWTEventListener(new AWTEventListener() {  
+			public void eventDispatched(AWTEvent e) { 
 				if(e.toString().contains("MOUSE_PRESSED")) {
 					mousePressed = true;
 					mouseReleased = false;
 					shouldUpdatePos = true;
 				}
-				
 				if(e.toString().contains("MOUSE_RELEASED")) {
 					mousePressed = false;
 					mouseReleased = true;
+					// upon a mouse release, we know that the mouse has been clicked (but, we have yet to
+					// determine whether this click was a drag)
 					hasBeenClicked = true;
 				}
-				
 			}  
 		}, eventMask);  
-
 	}
-	
-/*	private int getXCor(AWTEvent e) {
-		int absolute = e.toString().indexOf("absolute");
-		int firstParenth = e.toString().indexOf("(", absolute);
-		int endComma = e.toString().indexOf(",", firstParenth);
-		String xCor = e.toString().substring(firstParenth + 1, endComma);
-		System.out.println(xCor);
-		return Integer.parseInt(xCor);
-	}
-	
-	private int getYCor(AWTEvent e) {
-		int absolute = e.toString().indexOf("absolute");
-		int firstParenth = e.toString().indexOf("(", absolute);
-		int firstComma = e.toString().indexOf(",",firstParenth);
-		int endParenth = e.toString().indexOf(")", firstParenth);
-		String yCor = e.toString().substring(firstComma + 1, endParenth);
-		System.out.println(yCor);
-		return Integer.parseInt(yCor);
-	}*/
 
 	public static void main (String [] args) {
 		GUIAutomation automation = new GUIAutomation();
-		AuthoringView view = new AuthoringView();
+		AuthoringView view = new AuthoringView(); // MAKE INSTANCE OF YOUR OWN VIEW HERE
 		automation.initMouse();
-		automation.allowStop(null);
+		automation.allowStop();
 		automation.initRobot();
 		automation.record();
 
