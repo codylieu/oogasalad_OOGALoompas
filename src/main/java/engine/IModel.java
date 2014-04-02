934fbcf0acbba7ebd13d2871d8d0e50aea75b5f2
Lawrence Lin <justlah@gmail.com>
2014-04-01 21:20:07 -0400

    Add basic javadocs to interface classes

--- src/main/java/engine/IModel.java
+++ src/main/java/engine/IModel.java
@@ -1,9 +1,34 @@
 package main.java.engine;
 
-import main.java.engine.objects.CollisionManager;
+import java.util.List;
 
+import main.java.engine.objects.CollisionManager;
+import main.java.engine.objects.TDObject;
+
+/**
+ * An interface for TD game model
+ * @author lawrencelin
+ *
+ */
 public interface IModel {
 
-	public void setCollisionManager(CollisionManager cm);
+	/**
+	 * Set the collision manager of this model
+	 * see <code>CollisionManager</code> for how
+	 * the collision manager works
+	 * @param collisionManager
+	 */
+	public void setCollisionManager(CollisionManager collisionManager);
+	
+	/** Add a player to this game model
+	 * @param player
+	 */
+	public void addPlayer(IPlayer player);
 
+	/**
+	 * Get the list of TDObjects associated with
+	 * this game model
+	 * @return list of TDobjects
+	 */
+	public List<TDObject> getListOfObjects();
 }
--- src/main/java/engine/IPlayer.java
+++ src/main/java/engine/IPlayer.java
@@ -2,10 +2,43 @@ package main.java.engine;
 
 import main.java.engine.objects.CollisionManager;
 
+/** 
+ * A TD player must support the methods of this interface. 
+ * Each player maintains a list of game stats and dynamically
+ * keep track of them. 
+ * Possible players: human player and AI player
+ * @author lawrencelin
+ *
+ */
 public interface IPlayer {
 	
+	/** 
+	 * Get the score of this player. 
+	 * @return current score of the player
+	 */
 	public double getScore();
 	
+	/** 
+	 * Get the health of this player
+	 * @return current health level of player
+	 */
+	public double getHealth();
+	
+	/**
+	 * Call to update current stats associated
+	 * with the player
+	 */
 	public void updateGameStats();
-
+	
+	/**
+	 * Return the name of the player
+	 * @return the name of this player
+	 */
+	public String getName();
+	
+	/**
+	 * Check whether the game is lost
+	 * @return true if the player is dead
+	 */
+	public boolean isGameLost();
 }
