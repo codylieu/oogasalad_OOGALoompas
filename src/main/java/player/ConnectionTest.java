f7a50f173d3b872a47770e17c412a44929863b0d
Kevin Do <kevin.kydat.do@gmail.com>
2014-04-03 14:39:30 -0400

    Updated ConnectionTest

--- src/main/java/player/ConnectionTest.java
+++ src/main/java/player/ConnectionTest.java
@@ -7,7 +7,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class ConnectionTest {
 	public static void main(String[] args) {
@@ -45,32 +46,9 @@ public class ConnectionTest {
 	}
 
 	public static void downloadFromUrl(URL url, String localFilename) throws IOException {
-		InputStream is = null;
-		FileOutputStream fos = null;
-
-		try {
-			URLConnection urlConn = url.openConnection();//connect
-
-			is = urlConn.getInputStream();               //get connection inputstream
-			fos = new FileOutputStream(localFilename);   //open outputstream to local file
-
-			byte[] buffer = new byte[4096];              //declare 4KB buffer
-			int len;
-
-			//while we have availble data, continue downloading and storing to local file
-			while ((len = is.read(buffer)) > 0) {  
-				fos.write(buffer, 0, len);
-			}
-		} finally {
-			try {
-				if (is != null) {
-					is.close();
-				}
-			} finally {
-				if (fos != null) {
-					fos.close();
-				}
-			}
-		}
+		ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+		FileOutputStream fos = new FileOutputStream(localFilename);
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		fos.close();
 	}
 }
