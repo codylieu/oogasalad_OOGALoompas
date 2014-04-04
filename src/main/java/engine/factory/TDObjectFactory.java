8f87e9ce0310fab1136f10b0b0e69fabd78e0a37
Jordan ly <jordan.ly8@gmail.com>
2014-04-04 12:28:57 -0400

    Added documenation comments, removed extraneous Reflection class

--- src/main/java/engine/factory/TDObjectFactory.java
+++ src/main/java/engine/factory/TDObjectFactory.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 import main.java.engine.objects.tower.Tower;
-import main.java.exceptions.engine.MonsterCreationFailureException;
 import main.java.exceptions.engine.TowerCreationFailureException;
 import main.java.schema.SimpleTowerSchema;
 import main.java.schema.TowerSchema;
 import jgame.impl.JGEngineInterface;
+import main.java.util.Reflection;
 
 public class TDObjectFactory {
 	private JGEngineInterface engine;
@@ -43,15 +43,20 @@ public class TDObjectFactory {
 	}
 
 	private Object placeObject(String objectPackageLocation, String concreteName, Object[] parameters) {
-		return Reflection.createInstance(objectPackageLocation+concreteName, parameters);
+		return Reflection.createInstance(objectPackageLocation + concreteName, parameters);
 	}
-	
+
+    /**
+     * Find the top-left corner associated with the tile associated with the given location. Used to place
+     * new objects and images.
+     *
+     * @param location Coordinate of the map used to find the associated file
+     * @return The top left corner of the tile at the given coordinate
+     */
 	private Point2D findTileOrigin(Point2D location) {
 		int curXTilePos = (int) location.getX()/engine.tileWidth() * engine.tileWidth();
 		int curYTilePos = (int) location.getY()/engine.tileHeight() * engine.tileHeight();
 
 		return new Point2D.Double(curXTilePos, curYTilePos);
 	}
-
-
 }
