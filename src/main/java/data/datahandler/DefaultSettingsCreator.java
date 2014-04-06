4d77b06ed0ebe8a55e940157b729608d01aabad2
Jimmy Fang <jimmy.fang@duke.edu>
2014-04-06 13:42:27 -0400

    Finished implementation, will have to change if they change format of schemas. Also can't progress further without resolving waveschema issue

--- src/main/java/data/datahandler/DefaultSettingsCreator.java
+++ src/main/java/data/datahandler/DefaultSettingsCreator.java
@@ -18,14 +18,28 @@ import main.java.schema.GameBlueprint;
  */
 public class DefaultSettingsCreator {
 	private GameBlueprint myGameBlueprint;
+	//private List<WaveSpawnSchema> myWaveSpawnSchemas;
+	private int myStartingMoney;
+	private int myStartingLives;
 	
 	// Things the gamestate constructor takes in
 	//List<Monster> currentMonsters, Tower[][] currentTowers, Point2D currentEntrance, 
 	//Point2D currentExit, int currentWaveNumber, List<WaveSpawnSchema> allCurrentWaves, double currentGameClock, 
 	//int playerMoney, int playerLife, double playerScore
 	
+	/**
+	 * Constructor takes in a gameblueprint, stores all the relevant
+	 * value as its fields, and opens the option to create
+	 * the Relevant GameState, package it with the GameBlueprint,
+	 * and it on to player/engine.
+	 * @param gameBlueprint
+	 */
 	public DefaultSettingsCreator(GameBlueprint gameBlueprint){
 		myGameBlueprint = gameBlueprint;
+		//myWaveSpawnSchemas = myGameBlueprint.getMyLevelSchemas(); need to figure out this, currently in engine??
+		myStartingMoney = myGameBlueprint.getMyGameScenario().getMyStartingGold();
+		myStartingLives = myGameBlueprint.getMyGameScenario().getMyStartingLives();
+		
 	}
 	
 	/**
@@ -36,7 +50,7 @@ public class DefaultSettingsCreator {
 	 * @return a default GameState from a GameBlueprint
 	 */
 	public GameState createDefaultGameState(){
-		return new GameState(null,null,null,null,1,myGameBlueprint.getMyLevelSchemas())
+		//return new GameState(null,null,null,null,1,myGameBlueprint.getMyLevelSchemas(),0,null,myStartingMoney,myStartingLives,0)
 		return null;
 	}
 	
