8cb2db9b9433cfc87f7361612032772cd23bb6a7
Jordan ly <jordan.ly8@gmail.com>
2014-04-09 19:29:03 -0400

    Temporary fix for player, now works as usual

--- src/main/java/player/TowerGhost.java
+++ src/main/java/player/TowerGhost.java
@@ -5,7 +5,7 @@ import jgame.JGObject;
 public class TowerGhost extends JGObject {
 	
 	public TowerGhost(double x, double y) {
-		super("TowerGhost", false, x, y, 0, "SimpleTower");
+		super("TowerGhost", false, x, y, 0, "test-tower-1");
 		//expiry = 1;
 	}
 }
