f60e47b0ddc01b09ee4a1e1c3291abcac11dc08f
Jimmy Fang <jimmy.fang@duke.edu>
2014-04-11 02:43:01 -0400

    working on decompress, reworking unzip

--- src/main/java/data/datahandler/DataHandler.java
+++ src/main/java/data/datahandler/DataHandler.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,8 +14,11 @@ import java.util.List;
 
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.io.ZipInputStream;
 import net.lingala.zip4j.io.ZipOutputStream;
+import net.lingala.zip4j.model.FileHeader;
 import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.unzip.UnzipUtil;
 import net.lingala.zip4j.util.Zip4jConstants;
 
 import main.java.engine.GameState;
@@ -30,6 +34,7 @@ import com.google.gson.Gson;
 public class DataHandler {
 	private final static String FILE_PATH = "src/test/resources"; // change back to src/main/resources after implementation is done!
 	private final static String TEST_FILE_PATH = "src/test/resources.replacement.tester";
+	private final static int BUFF_SIZE = 4096;
 
 	/*private Gson myGson;
 
@@ -154,25 +159,24 @@ public class DataHandler {
 	 * @throws ClassNotFoundException 
 	 * @throws ZipException 
 	 */
-//	public GameBlueprint loadBlueprint(String filePath) throws ClassNotFoundException, IOException, ZipException {
-//		Object unserializedObject = loadObjectFromFile(filePath);
-//
-//		if (unserializedObject instanceof DataBundle) {
-//			DataBundle bundle = ((DataBundle) loadObjectFromFile(filePath));
-//			//ZipFile myZippedResources = bundle.getZippedResourcesFolder();
-//			String myZippedResourcesLocation = bundle.getZippedResourcesFolderLocation();
-//			File myDir = new File(TEST_FILE_PATH);
-//			deleteDirectory(myDir);
-//			ZipFile myZippedResourcesFolder = new ZipFile(myZippedResourcesLocation);
-//			//unzip and put resources in src/main/resources
-//			decompress(myZippedResourcesFolder,TEST_FILE_PATH); 
-//			return bundle.getBlueprint();
-//		}
-//		throw new ClassNotFoundException("Not a data bundle");
-//	}
-	
+	//	public GameBlueprint loadBlueprint(String filePath) throws ClassNotFoundException, IOException, ZipException {
+	//		Object unserializedObject = loadObjectFromFile(filePath);
+	//
+	//		if (unserializedObject instanceof DataBundle) {
+	//			DataBundle bundle = ((DataBundle) loadObjectFromFile(filePath));
+	//			//ZipFile myZippedResources = bundle.getZippedResourcesFolder();
+	//			String myZippedResourcesLocation = bundle.getZippedResourcesFolderLocation();
+	//			File myDir = new File(TEST_FILE_PATH);
+	//			deleteDirectory(myDir);
+	//			ZipFile myZippedResourcesFolder = new ZipFile(myZippedResourcesLocation);
+	//			//unzip and put resources in src/main/resources
+	//			decompress(myZippedResourcesFolder,TEST_FILE_PATH); 
+	//			return bundle.getBlueprint();
+	//		}
+	//		throw new ClassNotFoundException("Not a data bundle");
+	//	}
+
 	public GameBlueprint loadBlueprint(String filePath) throws ClassNotFoundException, IOException, ZipException {
-		Object unserializedObject = loadObjectFromFile(filePath);
 
 		if (unserializedObject instanceof DataBundle) {
 			DataBundle bundle = ((DataBundle) loadObjectFromFile(filePath));
@@ -213,21 +217,104 @@ public class DataHandler {
 
 	/**
 	 * Unzips a ZIP file to a target location
+	 * using input stream.
 	 * @param compressedFile
 	 * @param destination
 	 */
 
-	private static void decompress(ZipFile zippedResources,String destination) {
+	//	private static void decompress(ZipFile zippedResources,String destination) {
+	//		try {
+	//			if (zippedResources.isEncrypted()) {
+	//				//zipFile.setPassword(password);
+	//			}
+	//			zippedResources.extractAll(destination);
+	//		} catch (ZipException e) {
+	//			e.printStackTrace();
+	//		}
+	//
+	//		System.out.println("File Decompressed");
+	//	}
+
+	private static void decompress(String zipFileLocation,String destinationPath) {
 		try {
-			if (zippedResources.isEncrypted()) {
-				//zipFile.setPassword(password);
+			// Initiate the ZipFile
+			ZipFile zipFile = new ZipFile(zipFileLocation);
+
+			//Get a list of FileHeader. FileHeader is the header information for all the
+			//files in the ZipFile
+			List fileHeaderList = zipFile.getFileHeaders();
+
+			// Loop through all the fileHeaders
+			for (int i = 0; i < fileHeaderList.size(); i++) {
+				FileHeader fileHeader = (FileHeader)fileHeaderList.get(i);
+				if (fileHeader != null) {
+					
+					String outFilePath = destinationPath + System.getProperty("file.separator") + fileHeader.getFileName();
+					File outFile = new File(outFilePath);
+
+					//Checks if the file is a directory
+					if (fileHeader.isDirectory()) {
+						//This functionality is up to your requirements
+						//For now I create the directory
+						outFile.mkdirs();
+						continue;
+					}
+
+					//Check if the directories(including parent directories)
+					//in the output file path exists
+					File parentDir = outFile.getParentFile();
+					if (!parentDir.exists()) {
+						parentDir.mkdirs();
+					}
+
+					//Get the InputStream from the ZipFile
+					ZipInputStream is = zipFile.getInputStream(fileHeader);
+					//Initialize the output stream
+					OutputStream os = new FileOutputStream(outFile);
+
+					int readLen = -1;
+					byte[] buff = new byte[BUFF_SIZE];
+
+					//Loop until End of File and write the contents to the output stream
+					while ((readLen = is.read(buff)) != -1) {
+						os.write(buff, 0, readLen);
+					}
+
+					//Please have a look into this method for some important comments
+					closeFileHandlers(is, os);
+
+					//To restore File attributes (ex: last modified file time, 
+					//read only flag, etc) of the extracted file, a utility class
+					//can be used as shown below
+					UnzipUtil.applyFileAttributes(fileHeader, outFile);
+
+					System.out.println("Done extracting: " + fileHeader.getFileName());
+				} else {
+					System.err.println("fileheader is null. Shouldn't be here");
+				}
 			}
-			zippedResources.extractAll(destination);
-		} catch (ZipException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		System.out.println("File Decompressed");
+	}
+	
+	private static void closeFileHandlers(ZipInputStream is, OutputStream os) throws IOException{
+		//Close output stream
+		if (os != null) {
+			os.close();
+			os = null;
+		}
+		
+		//Closing inputstream also checks for CRC of the the just extracted file.
+		//If CRC check has to be skipped (for ex: to cancel the unzip operation, etc)
+		//use method is.close(boolean skipCRCCheck) and set the flag,
+		//skipCRCCheck to false
+		//NOTE: It is recommended to close outputStream first because Zip4j throws 
+		//an exception if CRC check fails
+		if (is != null) {
+			is.close();
+			is = null;
+		}
 	}
 
 	/**
