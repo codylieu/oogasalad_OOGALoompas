9634f5ce268f7380b784cb580d49944602f21423
codylieu <cody.a.lieu@gmail.com>
2014-04-17 02:34:48 -0400

    Implemented removal of most recent wave

--- src/main/java/author/view/tabs/wave_editor/WaveEditorTab.java
+++ src/main/java/author/view/tabs/wave_editor/WaveEditorTab.java
@@ -22,110 +22,113 @@ import main.java.author.view.tabs.EditorTab;
 import main.java.schema.WaveSpawnSchema;
 
 public class WaveEditorTab extends EditorTab {
-	
+
 	private List<WaveSpawnSchema> myWaves;
-	
+
 	String[] columnNames = {"Waves", "Monster 1", "Monster 2", "Monster 3"};
-	
+
 	Object[][] data = {};
-	
+
 	JTable table;
-	
-	private static int NUMBER_OF_WAVES = 1;
+
+	private static int NUMBER_OF_WAVES = 0;
 
 	public WaveEditorTab(TabController tabController) {
 		super(tabController);
 		add(createWaveEditorContent(), BorderLayout.CENTER);
 	}
-	
+
 	public JComponent createWaveEditorContent(){
-		
+
 		JPanel content = new JPanel(new BorderLayout());
-		
+
 		content.add(createTable(), BorderLayout.NORTH);
 		content.add(makeButtons(), BorderLayout.SOUTH);
-		
+
 		return content;
 	}
-	
+
 	public JComponent createTable(){
-		
+
 		table = new JTable(new DefaultTableModel(data, columnNames));
-		
+
 		JScrollPane sp = new JScrollPane(table);
-		
+
 		return sp;
 	}
-	
+
 	private JComponent makeButtons(){
-		
+
 		JPanel panel = new JPanel(new GridLayout(0, 1));
-		
+
 		panel.add(makeNewWaveButton(), BorderLayout.NORTH);
 		panel.add(makeRemoveMostRecentWaveButton(), BorderLayout.CENTER);
 		panel.add(makeRemoveWaveButton(), BorderLayout.SOUTH);
-		
+
 		return panel;
 	}
 
 	private JComponent makeNewWaveButton(){
-		
+
 		JButton addNewWaveButton = new JButton("Add New Wave");
-		
+
 		addNewWaveButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.addRow(new Object[]{"Wave " + NUMBER_OF_WAVES, new Integer(0), new Integer(0), new Integer(0)});
 				NUMBER_OF_WAVES++;
+				model.addRow(new Object[]{"Wave " + NUMBER_OF_WAVES, new Integer(0), new Integer(0), new Integer(0)});
+
 			}
 		});
-		
+
 		return addNewWaveButton;
 	}
-	
+
 	// Just here to test simpler case of removing rows
 	private JComponent makeRemoveMostRecentWaveButton(){
-		
+
 		JButton removeMostRecentWaveButton = new JButton("Remove Last Wave");
-		
+
 		removeMostRecentWaveButton.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.removeRow(NUMBER_OF_WAVES--);
-				NUMBER_OF_WAVES--;
+				if(NUMBER_OF_WAVES > 0){
+					NUMBER_OF_WAVES--;
+					model.removeRow(NUMBER_OF_WAVES);
+				}
 			}
-			
+
 		});
-		
+
 		return removeMostRecentWaveButton;
 	}
-	
+
 	// Currently does nothing, will figure it out later
 	private JComponent makeRemoveWaveButton(){
-		
+
 		JButton removeWaveButton = new JButton("Remove Wave");
-		
+
 		removeWaveButton.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				DefaultTableModel model = (DefaultTableModel) table.getModel();
-//				model.removeRow();
+				//				model.removeRow();
 			}
-			
+
 		});
-		
+
 		return removeWaveButton;
 	}
 
 	private void addWaveData() {
-		
+
 	}
 
 	/**
@@ -136,7 +139,7 @@ public class WaveEditorTab extends EditorTab {
 	 */
 	public void updateTable() {
 		WaveController waveController = (WaveController) myController;
-//		int numLevels = waveController.getNumLevels();
+		//		int numLevels = waveController.getNumLevels();
 		List<String> possibleEnemies = waveController.getEnemyList();
 		//do stuff with table
 	}
@@ -144,7 +147,7 @@ public class WaveEditorTab extends EditorTab {
 	@Override
 	public void saveTabData() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
