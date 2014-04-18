ede46c8882fe621c63da77e8f05ffbc9db58c07f
dianwen <dianwen@gmail.com>
2014-04-18 02:07:45 -0400

    Added InstantFreeze and RowBomb schemas

---
+++ src/main/java/schema/tdobjects/items/InstantFreezeItemSchema.java
@@ -0,0 +1,48 @@
+package main.java.schema.tdobjects.items;
+
+import main.java.author.view.tabs.item.ItemViewConstants;
+import main.java.engine.objects.item.Annihilator;
+import main.java.engine.objects.item.AreaBomb;
+import main.java.engine.objects.item.InstantFreeze;
+import main.java.schema.tdobjects.ItemSchema;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * This is a settings object for a specific type of AreaBomb.
+ */
+public class InstantFreezeItemSchema extends ItemSchema {
+	public static final Class<InstantFreeze> MY_CONCRETE_TYPE = InstantFreeze.class;
+	public static final String FREEZE_DURATION = "Range";
+
+	public InstantFreezeItemSchema() {
+		super(MY_CONCRETE_TYPE);
+	}
+
+	/**
+	 * @param name name of monster
+	 */
+	public InstantFreezeItemSchema(String name) {
+		this();
+		populateDefaultAttributes(name);
+	}
+
+	public void populateDefaultAttributes(String name) {
+		addAttribute(ItemSchema.NAME, name);
+		addAttribute(ItemSchema.BUILDUP_TIME, ItemViewConstants.BUILDUP_DEFAULT);
+		addAttribute(ItemSchema.COST, ItemViewConstants.COST_DEFAULT);
+		addAttribute(ItemSchema.DAMAGE, ItemViewConstants.DAMAGE_DEFAULT);
+		addAttribute(ItemSchema.FLASH_INTERVAL, ItemViewConstants.FLASH_INTERVAL_DEFAULT);
+		addAttribute(ItemSchema.IMAGE_NAME, ItemViewConstants.IMAGE_DEFAULT);
+		addAttribute(InstantFreezeItemSchema.FREEZE_DURATION, ItemViewConstants.FREEZE_DURATION_DEFAULT);
+
+	}
+
+	@Override
+	protected Set<String> populateAdditionalAttributes() {
+		myAttributeSet.add(InstantFreezeItemSchema.FREEZE_DURATION);
+		return myAttributeSet;
+	}
+}
---
+++ src/main/java/schema/tdobjects/items/RowBombItemSchema.java
@@ -0,0 +1,44 @@
+package main.java.schema.tdobjects.items;
+
+import main.java.author.view.tabs.item.ItemViewConstants;
+import main.java.engine.objects.item.Annihilator;
+import main.java.engine.objects.item.AreaBomb;
+import main.java.schema.tdobjects.ItemSchema;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * This is a settings object for a specific type of AreaBomb.
+ */
+public class RowBombItemSchema extends ItemSchema {
+	public static final Class<AreaBomb> MY_CONCRETE_TYPE = AreaBomb.class;
+	public static final String RANGE = "Range";
+
+	public RowBombItemSchema() {
+		super(MY_CONCRETE_TYPE);
+	}
+
+	/**
+	 * @param name name of monster
+	 */
+	public RowBombItemSchema(String name) {
+		this();
+		populateDefaultAttributes(name);
+	}
+
+	public void populateDefaultAttributes(String name) {
+		addAttribute(ItemSchema.NAME, name);
+		addAttribute(ItemSchema.BUILDUP_TIME, ItemViewConstants.BUILDUP_DEFAULT);
+		addAttribute(ItemSchema.COST, ItemViewConstants.COST_DEFAULT);
+		addAttribute(ItemSchema.DAMAGE, ItemViewConstants.DAMAGE_DEFAULT);
+		addAttribute(ItemSchema.FLASH_INTERVAL, ItemViewConstants.FLASH_INTERVAL_DEFAULT);
+		addAttribute(ItemSchema.IMAGE_NAME, ItemViewConstants.IMAGE_DEFAULT);
+	}
+
+	@Override
+	protected Set<String> populateAdditionalAttributes() {
+		return new HashSet<String>(); // empty set, no new attributes
+	}
+}
