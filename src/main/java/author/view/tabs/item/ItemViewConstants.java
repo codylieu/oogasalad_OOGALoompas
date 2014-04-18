4632096b9c1790d83a62e0c9bf0006f3c2a20554
dianwen <dianwen@gmail.com>
2014-04-17 21:28:56 -0400

    Added item constants

---
+++ src/main/java/author/view/tabs/item/ItemViewConstants.java
@@ -0,0 +1,12 @@
+package main.java.author.view.tabs.item;
+
+import java.io.Serializable;
+
+public class ItemViewConstants {
+
+	public static final int ANNIHILATOR_FLASH_INTERVAL_DEFAULT = 5;
+	public static final double COST_DEFAULT = 20;
+	public static final double BUILDUP_DEFAULT = 5;
+	public static final double DAMAGE_DEFAULT = 5;
+	public static final int AREABOMB_FLASH_INTERVAL = 5;
+}
