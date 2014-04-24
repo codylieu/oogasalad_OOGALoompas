b38d279b47f190638e3ddf111d7883285f72a809
Austin Lu <austin.lu@duke.edu>
2014-04-24 16:48:58 -0400

    Changed out-of-scale default tower constants. This fixes 'flashing monster' bug

--- src/main/java/author/view/tabs/tower/TowerViewConstants.java
+++ src/main/java/author/view/tabs/tower/TowerViewConstants.java
@@ -13,10 +13,13 @@ public class TowerViewConstants {
 	public static final int DAMAGE_DEFAULT = 10;
 	public static final int BUILDUP_DEFAULT = 5;
 	public static final int RANGE_DEFAULT = 100;
-	public static final int MONEY_GRANTED_DEFAULT = 10;
-	public static final int FREEZE_SLOWDOWN_DEFAULT = 20;
+	public static final int MONEY_GRANTED_DEFAULT = 100;
+	/**
+	 * TODO: Change FREEZE_SLOWDOWN_DEFAULT to be a double. (it's a percentage)
+	 */
+	public static final int FREEZE_SLOWDOWN_DEFAULT = 1;
 	public static final Serializable FIRING_SPEED_DEFAULT = 3;
-	public static final Serializable MONEY_GRANT_INTERVAL_DEFAULT = 2;
+	public static final Serializable MONEY_GRANT_INTERVAL_DEFAULT = 100;
 	public static final String TOWER_BEHAVIOR_FREEZES = "Deals Frost Damage";
 	public static final String TOWER_BEHAVIOR_SHOOTS = "Can Shoot";
 	public static final String TOWER_BEHAVIOR_FARMS_MONEY = "Farms Money";
