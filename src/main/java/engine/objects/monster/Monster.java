51b5f0f46850ff23707b4605aa8d2b0bf5750a59
Austin Lu <austin.lu@duke.edu>
2014-04-24 23:23:58 -0400

    Make monster's freeze slowdown be a percentage of its previous speed

--- src/main/java/engine/objects/monster/Monster.java
+++ src/main/java/engine/objects/monster/Monster.java
@@ -2,164 +2,181 @@ package main.java.engine.objects.monster;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import jgame.JGColor;
 import jgame.JGPoint;
 import main.java.engine.objects.Exit;
 import main.java.engine.objects.TDObject;
-import main.java.engine.objects.monster.jgpathfinder.*;
+import main.java.engine.objects.monster.jgpathfinder.JGPath;
+import main.java.engine.objects.monster.jgpathfinder.JGPathfinder;
+import main.java.engine.objects.monster.jgpathfinder.JGPathfinderHeuristic;
+import main.java.engine.objects.monster.jgpathfinder.JGPathfinderInterface;
+import main.java.engine.objects.monster.jgpathfinder.JGTileMap;
+import main.java.engine.objects.monster.jgpathfinder.NoPossiblePathException;
 import main.java.schema.MonsterSpawnSchema;
 
+
 public abstract class Monster extends TDObject {
-	public static final int MONSTER_CID = 1;
+    private static final int PERCENT = 100;
 
-	protected double myHealth;
-	protected double myMoveSpeed;
-	protected double myMoneyValue;
-	protected JGPathfinderInterface myPathFinder;
-	protected Point2D myEntrance;
-	protected Exit myExit;
-	protected JGPath myPath;
-	protected String originalImage;
-	private MonsterSpawnSchema resurrectMonsterSchema;
+    public static final int MONSTER_CID = 1;
 
-	/* TODO: Clean up/move instance variables to appropriate concrete classes
-	 */
-	public Monster (//double x,
-			//double y,
-			Point2D entrance,
-			Exit exit,
-			Set<Integer> blocked,
-			double health,
-			double moveSpeed,
-			double rewardAmount,
-			String graphic,
-			MonsterSpawnSchema resurrectSchema) {
-		//TODO make factory add the spread between monsters in the same wave, and remove random from initial x,y
-		super("monster", entrance.getX() + Math.random() * 100, entrance.getY() + Math.random() * 100, MONSTER_CID, graphic);
-		myHealth = health;
-		myMoveSpeed = moveSpeed;
-		myMoneyValue = rewardAmount;
-		myEntrance = entrance;
-		myExit = exit;
-		myPathFinder = new JGPathfinder(new JGTileMap(eng, null, blocked), new JGPathfinderHeuristic()); // TODO: clean up
-		JGPoint pathEntrance = new JGPoint(eng.getTileIndex(x, y)); // TODO: move into diff method
-		JGPoint pathExit = new JGPoint(myExit.getCenterTile());
-		this.setSpeed(myMoveSpeed);
-		originalImage = graphic;
-		this.resurrectMonsterSchema = resurrectSchema;
-		try {
-			myPath = myPathFinder.getPath(pathEntrance, pathExit);
-		} catch (NoPossiblePathException e) {
-			e.printStackTrace();
-		}
-	}
+    protected double myHealth;
+    protected double myMoveSpeed;
+    protected double myMoneyValue;
+    protected JGPathfinderInterface myPathFinder;
+    protected Point2D myEntrance;
+    protected Exit myExit;
+    protected JGPath myPath;
+    protected String originalImage;
+    private MonsterSpawnSchema resurrectMonsterSchema;
 
-	@Override
-	public void move () {
-		if (this.xspeed != 0) {
-			if (myPath.peek() != null) {
-				JGPoint waypoint = eng.getTileCoord(myPath.peek());
+    /*
+     * TODO: Clean up/move instance variables to appropriate concrete classes
+     */
+    public Monster (// double x,
+    // double y,
+    Point2D entrance,
+                    Exit exit,
+                    Set<Integer> blocked,
+                    double health,
+                    double moveSpeed,
+                    double rewardAmount,
+                    String graphic,
+                    MonsterSpawnSchema resurrectSchema) {
+        // TODO make factory add the spread between monsters in the same wave, and remove random
+        // from initial x,y
+        super("monster", entrance.getX() + Math.random() * 100, entrance.getY() + Math.random() *
+                                                                100, MONSTER_CID, graphic);
+        myHealth = health;
+        myMoveSpeed = moveSpeed;
+        myMoneyValue = rewardAmount;
+        myEntrance = entrance;
+        myExit = exit;
+        myPathFinder =
+                new JGPathfinder(new JGTileMap(eng, null, blocked), new JGPathfinderHeuristic()); // TODO:
+                                                                                                  // clean
+                                                                                                  // up
+        JGPoint pathEntrance = new JGPoint(eng.getTileIndex(x, y)); // TODO: move into diff method
+        JGPoint pathExit = new JGPoint(myExit.getCenterTile());
+        this.setSpeed(myMoveSpeed);
+        originalImage = graphic;
+        this.resurrectMonsterSchema = resurrectSchema;
+        try {
+            myPath = myPathFinder.getPath(pathEntrance, pathExit);
+        }
+        catch (NoPossiblePathException e) {
+            e.printStackTrace();
+        }
+    }
 
-				// TODO: refactor, quick implementation to test - jordan
-				if (((int) (x + 10) >= waypoint.x && (int) (x - 10) <= waypoint.x) &&
-						((int) (y + 10) >= waypoint.y && (int) (y - 10) <= waypoint.y)) {
-					waypoint = myPath.getNext();
-				}
+    @Override
+    public void move () {
+        if (this.xspeed != 0) {
+            if (myPath.peek() != null) {
+                JGPoint waypoint = eng.getTileCoord(myPath.peek());
 
-				xdir = Double.compare(waypoint.x, x);
-				ydir = Double.compare(waypoint.y, y);
-				setDirSpeed(xdir, ydir, myMoveSpeed);
-			} else {
-				setSpeed(0);
-			}
-		}
-	}
+                // TODO: refactor, quick implementation to test - jordan
+                if (((int) (x + 10) >= waypoint.x && (int) (x - 10) <= waypoint.x) &&
+                    ((int) (y + 10) >= waypoint.y && (int) (y - 10) <= waypoint.y)) {
+                    waypoint = myPath.getNext();
+                }
 
-	/**
-	 *  Check if this object has died and should be removed
-	 */
-	public boolean isDead () {
-		return myHealth <= 0;
-	}
+                xdir = Double.compare(waypoint.x, x);
+                ydir = Double.compare(waypoint.y, y);
+                setDirSpeed(xdir, ydir, myMoveSpeed);
+            }
+            else {
+                setSpeed(0);
+            }
+        }
+    }
 
-	/**
-	 * Reduce the health of this object by a damage amount.
-	 * @param damage afflicting object's damage
-	 */
-	public void takeDamage (double damage) {
-		myHealth -= damage;
-	}
+    /**
+     * Check if this object has died and should be removed
+     */
+    public boolean isDead () {
+        return myHealth <= 0;
+    }
 
-	public double getOriginalSpeed() {
-		return myMoveSpeed;
-	}
+    /**
+     * Reduce the health of this object by a damage amount.
+     * 
+     * @param damage afflicting object's damage
+     */
+    public void takeDamage (double damage) {
+        myHealth -= damage;
+    }
 
-	public void reduceSpeed (double speed) {
-		myMoveSpeed *= speed;
-	}
+    public double getOriginalSpeed () {
+        return myMoveSpeed;
+    }
 
-	/**
-	 * Set the monster to be dead immediately, 
-	 * effectively removing it from the game
-	 */
-	public void setDead() {
-		myHealth = 0;
-		myMoneyValue = 0;
-	}
+    public void reduceSpeed (double speedPercentage) {
+        myMoveSpeed *= speedPercentage / PERCENT;
+    }
 
-	/**
-	 * Get money value received upon death of this monster
-	 * @return
-	 */
-	public double getMoneyValue() {
-		return myMoneyValue;
-	}
-	
-	/**
-	 * Get the original image of this monster
-	 * 
-	 * @return original image of the monster
-	 */
-	public String getOriginalImage() {
-		return originalImage;
-	}
+    /**
+     * Set the monster to be dead immediately,
+     * effectively removing it from the game
+     */
+    public void setDead () {
+        myHealth = 0;
+        myMoneyValue = 0;
+    }
 
-	@Override
-	public void paint() {
-		if (myPath != null) {
-			for (JGPoint p : myPath) {
-				JGPoint coord = eng.getTileCoord(p);
-				eng.drawOval(coord.x + eng.tileWidth()/2, coord.y + eng.tileHeight()/2,
-						10, 10, true, true, 10, JGColor.yellow);
-			}
-		}
-	}
+    /**
+     * Get money value received upon death of this monster
+     * 
+     * @return
+     */
+    public double getMoneyValue () {
+        return myMoneyValue;
+    }
 
-	/**
-	 * Get the schema that should be spawned upon the death of this monster.
-	 * Will be null if the monster has no resurrect schema to spawn upon death.
-	 * @return
-	 */
-	public MonsterSpawnSchema getResurrrectMonsterSpawnSchema() {
-		return resurrectMonsterSchema;
-	}
-	
-	/**
-	 * Get dynamic information about the monster
-	 * 
-	 * @return
-	 */
-	public List<String> getInfo() {
-		List<String> info = new ArrayList<String>();
-		info.add(""+x);
-		info.add(""+y);
-		info.add(""+myMoneyValue);
-		info.add(""+myHealth);
-		info.add(""+myMoveSpeed);
-		return info;
-	}
+    /**
+     * Get the original image of this monster
+     * 
+     * @return original image of the monster
+     */
+    public String getOriginalImage () {
+        return originalImage;
+    }
+
+    @Override
+    public void paint () {
+        if (myPath != null) {
+            for (JGPoint p : myPath) {
+                JGPoint coord = eng.getTileCoord(p);
+                eng.drawOval(coord.x + eng.tileWidth() / 2, coord.y + eng.tileHeight() / 2,
+                             10, 10, true, true, 10, JGColor.yellow);
+            }
+        }
+    }
+
+    /**
+     * Get the schema that should be spawned upon the death of this monster.
+     * Will be null if the monster has no resurrect schema to spawn upon death.
+     * 
+     * @return
+     */
+    public MonsterSpawnSchema getResurrrectMonsterSpawnSchema () {
+        return resurrectMonsterSchema;
+    }
+
+    /**
+     * Get dynamic information about the monster
+     * 
+     * @return
+     */
+    public List<String> getInfo () {
+        List<String> info = new ArrayList<String>();
+        info.add("" + x);
+        info.add("" + y);
+        info.add("" + myMoneyValue);
+        info.add("" + myHealth);
+        info.add("" + myMoveSpeed);
+        return info;
+    }
 }
--- src/main/java/engine/objects/projectile/FreezeProjectile.java
+++ src/main/java/engine/objects/projectile/FreezeProjectile.java
@@ -10,6 +10,9 @@ public class FreezeProjectile extends TDObject {
     public static final int TOWER_PROJECTILE_CID = 10;
     public static final double DEFAULT_SPEED = 20;
 
+    /**
+     * This is number is the % of which a monster's speed becomes once hit
+     */
     private double mySlowdownSpeed;
 
     /**
